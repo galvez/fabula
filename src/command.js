35cf2890b0c257445e2e5c46a7a307b8c1671d12
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-23 22:53:23 -0200

    lint

--- src/command.js
+++ src/command.js
@@ -20,7 +20,7 @@ export default class Command {
     const continueCommand = this.cmd.line.call(this, line)
     if (this.firstLine) {
       this.firstLine = false
-    }    
+    }
     this.source.push(line)
     return continueCommand
   }
