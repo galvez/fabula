ce854dcf69e83aeb25609a41c733404720764fbf
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-24 22:27:29 -0200

    add to comments src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -18,7 +18,8 @@ function loadConfig() {
     rcFile = null
   }
   if (rcFile === null) {
-    throw new Error('Fabula configuration not found.')
+    consola.fatal('Fabula configuration not found.')
+    process.exit()
   }
   return require(resolvePath(rcFile)).default
 }
@@ -27,10 +28,15 @@ export default async function () {
   const config = loadConfig()
   const args = arg({})
   if (args._.length === 2) {
+    // Run on remote servers:
+    // fabula <server1,server2,..> <script>
+    // fabula all <script>
     const servers = args._[0].split(/,/g)
     const source = args._[1]
     await run(source, config, servers)
   } else if (args._.length === 1) {
+    // Run strictly locally (non-local commands will cause an error)
+    // fabula <local-script>
     const source = args._[0]
     if (!source) {
       consola.fatal('No source specified.')
