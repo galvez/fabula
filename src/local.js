05c102b861cbecc4e42fdfa6571700b33629747c
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-29 06:47:58 -0200

    lint

--- src/local.js
+++ src/local.js
@@ -1,7 +1,6 @@
 import { writeFileSync, appendFileSync } from 'fs'
 import { promisify } from 'util'
 import { exec } from 'child_process'
-import { quote } from './utils'
 
 execLocal.exec = promisify(exec)
 
@@ -9,10 +8,10 @@ export function execLocal(cmd, env = {}) {
   return execLocal.exec(cmd, { env, shell: '/bin/bash' })
 }
 
-export async function localWrite(filePath, fileContents) {
+export function localWrite(filePath, fileContents) {
   writeFileSync(filePath, fileContents)
 }
 
-export async function localAppend(filePath, fileContents) {
+export function localAppend(filePath, fileContents) {
   appendFileSync(filePath, fileContents)
 }
