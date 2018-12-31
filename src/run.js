f63a4da5160321d947e1a3096565ddcbdcbcbdf3
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-31 20:12:08 -0200

    rework logging api

--- src/run.js
+++ src/run.js
@@ -3,65 +3,26 @@ import { parse } from 'path'
 import { compile } from './compile'
 import { createLogger } from './logging'
 
-export async function runLocalString(name, str, settings, logger) {
+export async function runLocalSource(name, str, settings, logger) {
   const commands = compile(name, str, settings)
-  logger.setContext('global', 'local')
   for (const command of commands) {
-    try {
-      if (!command.local) {
-        logger.info('[FAIL]', command.source[0])
-        logger.fatal('No servers specified to run this remote command.')
-        process.exit()
-      }
-      const response = await command.run()
-      if (response) {
-        if (response.stdout) {
-          for (const line of response.stdout.split(/\n/g).filter(Boolean)) {
-            logger.info(line.trim())
-          }
-        }
-        if (response.stderr) {
-          for (const line of response.stderr.trim().split(/\n/g).filter(Boolean)) {
-            logger.error(line.trim())
-          }
-        }
-      }
-      logger.info('[OK]', command.source[0])
-    } catch (err) {
+    if (!command.local) {
       logger.info('[FAIL]', command.source[0])
-      logger.fatal(err)
-      break
+      logger.fatal('No servers specified to run this remote command.')
+      process.exit()
+    }
+    if (await command.run(null, logger)) {
+      process.exit()
     }
   }
 }
 
-export async function runString(server, conn, name, str, settings, logger) {
-  logger.setContext('global', 'ssh', server)
-  settings = {
-    ...settings,
-    $server: conn.settings
-  }
+export async function runSource(server, conn, name, str, settings, logger) {
+  settings = { $server: conn.settings, ...settings }
   const commands = compile(name, str, settings)
   for (const command of commands) {
-    try {
-      const response = await command.run(conn)
-      if (response) {
-        if (response.stdout) {
-          for (const line of response.stdout.split(/\n/g).filter(Boolean)) {
-            logger.info(`[${server}]`, line.trim())
-          }
-        }
-        if (response.stderr) {
-          for (const line of response.stderr.trim().split(/\n/g).filter(Boolean)) {
-            logger.error(`[${server}]`, line.trim())
-          }
-        }
-      }
-      logger.info(`[${server}] [OK]`, command.source[0])
-    } catch (err) {
-      logger.error(`[${server}] [FAIL]`, command.source[0])
-      logger.fatal(err)
-      break
+    if (await command.run(conn, logger)) {
+      process.exit()
     }
   }
 }
