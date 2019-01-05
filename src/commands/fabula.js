26270105385b422368456707880c17b1b09cb705
Jonas Galvez <jonasgalvez@gmail.com>
2019-01-05 18:24:35 -0200

    lint

--- src/commands/fabula.js
+++ src/commands/fabula.js
@@ -7,7 +7,7 @@ export default {
     let match
     // eslint-disable-next-line no-cond-assign
     if (match = line.match(/^fabula\s+\.\s+([^ ]+?)\s*$/)) {
-        this.params.filePath = this.match[1]
+      this.params.filePath = this.match[1]
       return match
     }
   },
@@ -17,7 +17,7 @@ export default {
       fail: true
     }
     const commands = readFile(this.params.filePath).toString()
-    if (this.local) { 
+    if (this.local) {
       await runLocalSource(this.settings.$name, commands, settings, logger)
     } else {
       await runSource(this.context.server, conn, this.settings.$name, commands, settings, logger)
