fa9450a9cb2467aa7b10b6a59e26df09fe7a2cc6
Jonas Galvez <jonasgalvez@gmail.com>
2019-01-05 12:07:58 -0200

    improve mock bin

--- test/bin.js
+++ test/bin.js
@@ -1,23 +1,33 @@
-void function main() {
+module.exports = function main (shell = true, argv = null) {
   const
     args = require('arg')({
       '--code': Number,
       '--stdout': String,
       '--stderr': String,
-    }),
-    code = args['--code'] || 0,
+    }, { argv: argv || process.argv }),
     stdout = args['--stdout'],
-    stderr = args['--stderr']
+    stderr = args['--stderr'],
+    result = {
+      code: args['--code'] || 0
+    }
 
   if (stdout) {
-    process.stdout.write(`${stdout}\n`)
+    result.stdout = `${stdout}\n`
   }
-
   if (stderr) {
-    process.stdout.write(`${stderr}\n`)
-  } else if (code) {
-    process.stdout.write(`Exit code: ${code}\n`)
+    result.stderr = `${stderr}\n`
+  } else if (result.code) {
+    result.stderr = `exit code: ${result.code}\n`
+  }
+  if (!shell) {
+    return result
   }
 
-  process.exit(code)
+  if (result.stdout) {
+    process.stdout.write(result.stdout)
+  }
+  if (result.stderr || result.code) {
+    process.stderr.write(result.stderr)
+  }
+  process.exit(result.code)
 }()
