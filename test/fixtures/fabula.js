8ba47b2829dea0bee3199cbec897430933e377fb
Jonas Galvez <jonasgalvez@gmail.com>
2019-01-06 00:37:54 -0200

    update tests

--- test/fixtures/fabula.js
+++ test/fixtures/fabula.js
@@ -1,16 +1,35 @@
-export default {
-  logs: {
-    global: 'logs/global-log.log',
-    local: 'logs/local-log.log',
-    ssh: 'logs/ssh-log.log'
-  },
-  env: {
-    GLOBAL_VAR: 'GLOBAL_VAR',
-    local: {
-      GLOBAL_LOCAL_VAR: 'GLOBAL_LOCAL_VAR'
-    },
+import { launchTestSSHServer } from '../server'
+
+export default async function() {
+  const [
+    server1,
+    server2
+  ] = await Promise.all([
+    launchTestSSHServer(),
+    launchTestSSHServer()
+  ])
+  return {
     ssh: {
-      GLOBAL_SSH_VAR: 'GLOBAL_SSH_VAR'
+      server1: server1.settings,
+      server2: server2.settings
+    },
+    done() {
+      server1.server.close()
+      server2.server.close()
+    },
+    logs: {
+      global: 'logs/global-log.log',
+      local: 'logs/local-log.log',
+      ssh: 'logs/ssh-log.log'
+    },
+    env: {
+      GLOBAL_VAR: 'GLOBAL_VAR',
+      local: {
+        GLOBAL_LOCAL_VAR: 'GLOBAL_LOCAL_VAR'
+      },
+      ssh: {
+        GLOBAL_SSH_VAR: 'GLOBAL_SSH_VAR'
+      }
     }
   }
 }
--- test/server.js
+++ test/server.js
@@ -2,7 +2,7 @@
 const 
   // Standard library imports
   fs = require('fs'),
-  { resolve } = require('fs'),
+  { resolve } = require('path'),
   crypto = require('crypto'),
   // Node module imports
   buffersEqual = require('buffer-equal-constant-time'),
@@ -11,14 +11,14 @@ const
   getPort = require('get-port'),
   // Local imports
   { parseArgv } = require('../src/command'),
-  bin = require('./bin)',
+  bin = require('./bin'),
   // Settings
   privateKey = resolve(__dirname, 'fixtures', 'keys', 'ssh.private'),
   passphrase = 'fabula'
 
 function genPublicKey() {
   const pubKey = fs.readFileSync(resolve(__dirname, 'fixtures', 'keys', 'ssh.public'))
-  return ssh2.genPublicKey(ssh2.parseKey(pubKey))
+  return ssh2.utils.genPublicKey(ssh2.utils.parseKey(pubKey))
 }
 
 function authenticateSession(ctx) {
@@ -46,42 +46,37 @@ function authenticateSession(ctx) {
   }
 }
 
-function getServer() {
-  const
-    privateKey = resolve(__dirname, 'fixtures', 'keys', 'ssh.private'),
-    hostKeys = [fs.readFileSync(privateKey)]
-
-  return new Promise((resolve) => {
-    const server = new ssh2.Server({ hostKeys }, (client) => {
-      client.on('authentication', authenticateSession)
-      resolve({ server, client })
-    })
-  })
-}
-
 exports.launchTestSSHServer = async function() {
-  const { server, client } = await getServer()
 
-  client.on('session', (acceptSession) => {
-    acceptSession().session.once('shell', (acceptShell) => {
-      const stream = acceptShell()
-      stream.on('data', (data) => {
-        const argv = parseArgv(data.toString().trim())
-        if (argv[0] === 'bin') {
-          const result = bin(argv)
-          if (result.stdout) {
-            stream.write(result.stdout)
+  const privateKey = resolve(__dirname, 'fixtures', 'keys', 'ssh.private')
+  const hostKeys = [{
+    key: fs.readFileSync(privateKey),
+    passphrase
+  }]
+
+  const server = new ssh2.Server({ hostKeys }, (client) => {
+    client.on('authentication', authenticateSession)
+    client.on('session', (acceptSession) => {
+      acceptSession().session.once('shell', (acceptShell) => {
+        const stream = acceptShell()
+        stream.on('data', (data) => {
+          const argv = parseArgv(data.toString().trim())
+          if (argv[0] === 'bin') {
+            const result = bin(argv)
+            if (result.stdout) {
+              stream.write(result.stdout)
+            }
+            if (result.stderr) {
+              stream.stderr.write(result.stderr)
+            }
+            stream.exit(result.stderr)
+            stream.end()
+          } else {
+            stream.stderr.write('Command not recognized')
+            stream.exit(1)
+            stream.end()
           }
-          if (result.stderr) {
-            stream.stderr.write(result.stderr)
-          }
-          stream.exit(result.stderr)
-          stream.end()
-        } else {
-          stream.stderr.write('Command not recognized')
-          stream.exit(1)
-          stream.end()
-        }
+        })
       })
     })
   })
@@ -95,6 +90,7 @@ exports.launchTestSSHServer = async function() {
         server,
         settings: {
           hostname: address,
+          username: 'fabula',
           passphrase,
           privateKey,
           port
